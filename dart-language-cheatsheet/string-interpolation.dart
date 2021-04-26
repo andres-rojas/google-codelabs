b3834e76d9f7384f195504ab6abef97b89b534de
Andres Rojas <andres@conpat.io>
2021-04-25 21:14:46 -0700

    String interpolation

---
+++ dart-language-cheatsheet/string-interpolation.dart
@@ -0,0 +1,12 @@
+import './test.dart';
+
+void main() {
+  test(
+    'stringify returns a string containing both integers separated by a space',
+    stringify(2, 3) == '2 3',
+  );
+}
+
+String stringify(int x, int y) {
+  return '$x $y';
+}
