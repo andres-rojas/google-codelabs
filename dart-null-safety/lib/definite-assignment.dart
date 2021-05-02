87b03a488e454a02ba447d74a9113889ec3ff526
Andres Rojas <andres@conpat.io>
2021-05-02 16:02:44 -0700

    Definite assignment

---
+++ dart-null-safety/lib/definite-assignment.dart
@@ -0,0 +1,13 @@
+void main() {
+  String text;
+
+  // Comment in/out this block to see deinite assignment in action
+  if (DateTime.now().hour < 12) {
+    text = "It's morning! Let's make aloo paratha!";
+  } else {
+    text = "It's afternoon! Let's make biryani!";
+  }
+
+  print(text);
+  print(text.length);
+}
